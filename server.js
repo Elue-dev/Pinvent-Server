5db1f36a4bd22a789b73810e4909710f739e430d
Wisdom <eluewisdom@gmail.com>
2022-12-11 02:33:59 +0100

    update: dotenv init

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const handleAsync = require("./utils/handle_async");
 
-dotenv.load();
+dotenv.config();
 
 const app = require("./app");
 
