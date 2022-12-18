2b5fd03d43865e9834e4f0f072c8b30fb56a7acc
Wisdom <eluewisdom@gmail.com>
2022-12-18 18:39:46 +0100

    fix: wrong positioning in welcome message

--- utils/welcome_message.js
+++ utils/welcome_message.js
@@ -38,14 +38,14 @@ const welcome_message = (url, username) => {
                                       <td style="padding:0 35px;">
                                           <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif; text-align: left;">Welcome, ${username}!</h1>
                                           <span
-                                              style="display:flex; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                              style="display:innline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
                                               <span style='text-align:left;'>
                                               <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
                                           Welcome to Pinvent, we're glad to have you 🎉🙏.<br>
                                           We're all a big familiy here, so make sure to update your user profile details so we get to know you a bit better!
                                           </p>
                                           </span>
-                                          <span style='text-align:left;>
+                                          <span>
                                           <p>Regards, <br> Pinvent team.</p>
                                           </span>
                                           <span style='text-align: left'>
