f08ae78edbc6a1f63000b4fe5a971ae1f2c20ad8
Wisdom <eluewisdom@gmail.com>
2023-01-01 13:29:06 +0100

    build: run npm intall to try to fix stdin error

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://127.0.0.1:5173",
+      "http://127.0.0.1:5175",
       "https://pinvent-client.vercel.app",
       process.env.CLIENT_URL,
     ],
